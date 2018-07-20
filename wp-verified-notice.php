7b0b11c2dcb5ced63a4de038a995f987156290a1
Marc Tönsing <marc.toensing@gmail.com>
2018-07-20 18:06:55 +0200

    tabindex ignored

--- wp-verified-notice.php
+++ wp-verified-notice.php
@@ -4,7 +4,7 @@
 Plugin Name: WP Verified Notice
 Description: Displays a notice that verified accounts exists
 GitHub Plugin URI: mtoensing/wp-verified-notice
-Version:     1.8
+Version:     1.9
 Author:      MarcDK
 Author URI:  https://marc.tv
 License URI: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -17,7 +17,7 @@ function wpva_comment_form_defaults( $defaults ) {
 	$here = get_permalink();
 	$home_url = home_url();
 
-	$defaults['comment_notes_after'] = $defaults['comment_notes_after'] . '<p class="comment-notes verified-notice"><span class="marctv-verified"></span> <a rel="nofollow" href="https://marc.tv/?p=26784">Wie bekomme ich einen verifizierten Account?</a> - <a href="' . $home_url .  '/wp-login.php?redirect_to='. $here .'#respond">Login</a></p>';
+	$defaults['comment_notes_after'] = $defaults['comment_notes_after'] . '<p class="comment-notes verified-notice"><span class="marctv-verified"></span> <a rel="nofollow" tabindex="-1" href="https://marc.tv/?p=27967">Wie bekomme ich einen verifizierten Account?</a> - <a tabindex="-1" rel="nofollow" href="' . $home_url .  '/wp-login.php?redirect_to='. $here .'#respond">Login</a></p>';
 
 	return $defaults;
 }
